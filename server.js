8b356b2dc38841292e2134fae22622eff67d62ab
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-01-08 14:17:01 +0530

    Changed PORT to process.env.PORT or 5000.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var routes = require('./lib/routes');
 var server = http.createServer(routes);
-server.listen(5000);
+server.listen(process.env.PORT || 5000);
 server.on('error',function(e) {
 	console.log(e);
 });
