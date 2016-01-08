62705577590f6558145d439e16ceb106e8a878c2
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-01-08 14:03:53 +0530

    Changed port no to 5000.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var routes = require('./lib/routes');
 var server = http.createServer(routes);
-server.listen(3000);
+server.listen(5000);
 server.on('error',function(e) {
 	console.log(e);
 });
